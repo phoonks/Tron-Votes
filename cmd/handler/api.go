f31f657e68f2342569d19d8f762c8a0782d479e6
ksMerquri <kahseng.phoon@merquri.io>
2025-01-17 11:01:00 +0800

    fix: tron votes

--- cmd/handler/api.go
+++ cmd/handler/api.go
@@ -1,20 +1,31 @@
 package handler
 
 import (
+	"context"
+	"crypto/sha256"
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"sort"
 
-	// base58 "github.com/btcsuite/btcd/btcutil/base58"
-
+	"github.com/btcsuite/btcd/btcutil/base58"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/gin-gonic/gin"
 	cf "github.com/kahsengphoon/Tron-Votes/config"
+
+	"github.com/fbsobreira/gotron-sdk/pkg/client"
+	"github.com/fbsobreira/gotron-sdk/pkg/proto/core"
 	"github.com/urfave/cli/v2"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/proto"
 )
 
 const (
-	shastaDomain = "https://api.shasta.trongrid.io"
-	ownAddress   = "TKjxS5iEAzyJHY9g7ZJR8XwTshDUEeH379"
+	shastaDomain  = "grpc.shasta.trongrid.io:50051"
+	ownAddress    = "TKjxS5iEAzyJHY9g7ZJR8XwTshDUEeH379"
+	hexOwnAddress = "416b2fb069425a03935b9e39d372319cdc1a4fed9f"
+	privateKeyHex = "d1455980b1f333aba7a1abf852216abfeec8b95b230b6f5bf1af21c0e4a0c898"
 )
 
 func (h *HttpServer) StartApiServer(c *cli.Context) error {
@@ -33,6 +44,12 @@ func (h *HttpServer) StartApiServer(c *cli.Context) error {
 		Handler: r,
 	}
 
+	// freezeBalance()
+	// getVotePower()
+	getSpAddress()
+	// getClaimableReward()
+	// voteTransaction()
+
 	if err := r.Run(fmt.Sprintf(":%v", cf.Enviroment().AppServerPort)); err != nil {
 		return err
 	}
@@ -40,32 +57,210 @@ func (h *HttpServer) StartApiServer(c *cli.Context) error {
 	return nil
 }
 
-// func freezeBalance() {
-// 	client := resty.New()
+func freezeBalance() {
+	conn := client.NewGrpcClient(shastaDomain)
+	err := conn.Start(grpc.WithInsecure())
+	if err != nil {
+		fmt.Printf("fail to NewGrpcClient: %+v \n", conn)
+		return
+	}
+	defer conn.Stop()
 
-// 	// Convert Base58 address to Hex
-// 	ownerAddressHex, err := base58ToHex(ownAddress)
-// 	if err != nil {
-// 		log.Fatalf("Address conversion error: %v", err)
-// 	}
+	rawTx, err := conn.FreezeBalanceV2(ownAddress, core.ResourceCode_ENERGY, 1000000)
+	if err != nil {
+		fmt.Printf("fail to FreezeBalanceV2: %+v \n", err)
+		return
+	}
+	fmt.Println("rawTx:", rawTx.String())
 
-// 	// Define the freeze request
-// 	body := map[string]interface{}{
-// 		"owner_address":   ownerAddressHex, // Hex address
-// 		"frozen_balance":  1000000,         // Amount in SUN (1 TRX = 1,000,000 SUN)
-// 		"frozen_duration": 1,               // Freeze duration in days
-// 		"resource":        "ENERGY",        // Options: "ENERGY" or "BANDWIDTH"
-// 	}
+	signTransaction(rawTx.Transaction)
+	txBroad, err := conn.Broadcast(rawTx.Transaction)
+	if err != nil {
+		fmt.Printf("fail to Broadcast: %+v \n", err)
+		return
+	}
+	fmt.Println("txBroad:", txBroad.String())
+}
 
-// 	// Send the request
-// 	resp, err := client.R().
-// 		SetHeader("Content-Type", "application/json").
-// 		SetBody(body).
-// 		Post(shastaDomain + "/wallet/freezebalance")
+func signTransaction(tx *core.Transaction) (*core.Transaction, error) {
+	// Decode the private key from hex
+	privateKey, err := crypto.HexToECDSA(privateKeyHex)
+	if err != nil {
+		return nil, fmt.Errorf("invalid private key: %w", err)
+	}
 
-// 	if err != nil {
-// 		log.Fatalf("Error freezing balance: %v", err)
-// 	}
+	// Get the raw transaction hash
+	rawData, err := proto.Marshal(tx.GetRawData())
+	if err != nil {
+		return nil, fmt.Errorf("failed to serialize transaction: %w", err)
+	}
 
-// 	fmt.Println("Response:", resp.String())
-// }
+	hash := sha256.Sum256(rawData)
+
+	// Sign the hash with the private key
+	signature, err := crypto.Sign(hash[:], privateKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to sign hash: %w", err)
+	}
+
+	// Add the signature to the transaction
+	tx.Signature = append(tx.Signature, signature)
+	return tx, nil
+}
+
+func voteTransaction() {
+	conn := client.NewGrpcClient(shastaDomain)
+	err := conn.Start(grpc.WithInsecure())
+	if err != nil {
+		fmt.Printf("fail to NewGrpcClient: %+v \n", conn)
+		return
+	}
+	defer conn.Stop()
+
+	// Define the votes
+	votes := []*core.VoteWitnessContract_Vote{
+		{
+			VoteAddress: decodeAddress("TExYAkRVJsRdbAPfdFtGKPE7ZxCDopq8dQ"),
+			VoteCount:   34, // Number of votes for this SR
+		},
+		{
+			VoteAddress: decodeAddress("TVDuR9wjfhVDZYPB1YsTu5f5QVLxE2JqLS"),
+			VoteCount:   34, // Number of votes for this SR
+		},
+		{
+			VoteAddress: decodeAddress("TDcHeAMHPYfJKsknLCnV89K9wu8dqdD3rm"),
+			VoteCount:   33, // Number of votes for this SR
+		},
+	}
+
+	// Create the vote transaction
+	voteContract := &core.VoteWitnessContract{
+		OwnerAddress: decodeAddress(ownAddress),
+		Votes:        votes,
+	}
+	rawTx, err := conn.Client.VoteWitnessAccount2(context.Background(), voteContract)
+	if err != nil {
+		log.Fatalf("Failed to create vote transaction: %v", err)
+	}
+	fmt.Printf("Raw vote transaction created: %s\n", rawTx.String())
+
+	// Sign the transaction with the private key
+	signedTx, err := signTransaction(rawTx.Transaction)
+	if err != nil {
+		log.Fatalf("Failed to sign vote transaction: %v", err)
+	}
+	fmt.Printf("Signed vote transaction: %s\n", signedTx.String())
+
+	// Broadcast the signed transaction
+	result, err := conn.Broadcast(signedTx)
+	if err != nil {
+		log.Fatalf("Failed to broadcast vote transaction: %v", err)
+	}
+	fmt.Printf("Vote transaction broadcast result: %s\n", result.String())
+
+}
+
+// decodeAddress converts a base58 Tron address to a hex format
+func decodeAddress(base58Addr string) []byte {
+	decoded := base58.Decode(base58Addr)
+	if len(decoded) == 0 {
+		log.Fatalf("Failed to decode base58 address: %s", base58Addr)
+	}
+
+	// The last 4 bytes are the checksum, so we only take the first 21 bytes
+	return decoded[:len(decoded)-4]
+}
+
+func encodeAddress(decodeAddr []byte) string {
+	if len(decodeAddr) != 21 {
+		panic("Invalid raw address length. Expected 21 bytes.")
+	}
+
+	encoded := base58.CheckEncode(decodeAddr[1:], decodeAddr[0])
+	if len(encoded) == 0 {
+		log.Fatalf("Failed to decode base58 address: %s", decodeAddr)
+	}
+
+	// The last 4 bytes are the checksum, so we only take the first 21 bytes
+	return encoded
+}
+
+func getVotePower() {
+	// Connect to the Tron network
+	conn := client.NewGrpcClient(shastaDomain)
+	err := conn.Start(grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to connect to Tron network: %v", err)
+	}
+	defer conn.Stop()
+
+	// Query account information
+	account, err := conn.GetAccount(ownAddress)
+	if err != nil {
+		log.Fatalf("Failed to get account information: %v", err)
+	}
+
+	// Calculate total voting power
+	totalVotePower := int64(0)
+	for _, v := range account.FrozenV2 {
+		totalVotePower += v.Amount
+	}
+	totalVotePower = int64(float64(totalVotePower) / 1_000_000)
+
+	totalVotedPower := int64(0)
+	for _, v := range account.Votes {
+		totalVotedPower += v.VoteCount
+	}
+
+	fmt.Printf("Total Vote Power (in TRX): %d\n", totalVotePower)
+	fmt.Printf("Total Voted Power (in TRX): %d\n", totalVotedPower)
+}
+
+func getSpAddress() {
+	// Connect to the Tron network
+	conn := client.NewGrpcClient(shastaDomain)
+	err := conn.Start(grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to connect to Tron network: %v", err)
+	}
+	defer conn.Stop()
+
+	// Get SR list
+	witnessList, err := conn.ListWitnesses()
+	if err != nil {
+		log.Fatalf("Failed to get SR list: %v", err)
+	}
+
+	// Sort witnesses by VoteCount in descending order
+	sort.Slice(witnessList.Witnesses, func(i, j int) bool {
+		return witnessList.Witnesses[i].VoteCount > witnessList.Witnesses[j].VoteCount
+	})
+
+	// Print SR addresses
+	fmt.Println("List of Super Representatives:")
+	for _, witness := range witnessList.Witnesses {
+		fmt.Printf("Address: %s, URL: %s, Last Round Votes: %d, Total Produced: %d \n\n", encodeAddress(witness.Address), witness.Url, witness.VoteCount, witness.TotalProduced)
+	}
+}
+
+func getClaimableReward() {
+	// Initialize the gRPC client
+	conn := client.NewGrpcClient(shastaDomain)
+	err := conn.Start(grpc.WithInsecure())
+	if err != nil {
+		fmt.Printf("Failed to start gRPC client: %+v\n", err)
+		return
+	}
+	defer conn.Stop()
+
+	// Get Claimable Rewards
+	claimableRewards, err := conn.GetRewardsInfo(ownAddress)
+	if err != nil {
+		fmt.Printf("Error fetching rewards: %+v\n", err)
+		return
+	}
+
+	// Display Rewards
+	fmt.Printf("Claimable Rewards for address %s: %.6f TRX\n", ownAddress, float64(claimableRewards)/1_000_000)
+
+}
--- go.mod
+++ go.mod
@@ -3,43 +3,63 @@ module github.com/kahsengphoon/Tron-Votes
 go 1.23.3
 
 require (
+	github.com/btcsuite/btcd/btcutil v1.1.6
+	github.com/ethereum/go-ethereum v1.12.2
+	github.com/fbsobreira/gotron-sdk v0.0.0-20230907131216-1e824406fe8c
 	github.com/gin-gonic/gin v1.10.0
 	github.com/urfave/cli/v2 v2.27.5
+	google.golang.org/grpc v1.37.0
+	google.golang.org/protobuf v1.35.2
 
 )
 
 require (
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
+	github.com/deckarep/golang-set v1.8.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/holiman/uint256 v1.2.3 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
-	github.com/kr/pretty v0.3.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pborman/uuid v1.2.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rjeczalik/notify v0.9.3 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/shengdoushi/base58 v1.0.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/tyler-smith/go-bip39 v1.1.0 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
+	go.uber.org/atomic v1.6.0 // indirect
+	go.uber.org/multierr v1.5.0 // indirect
+	go.uber.org/zap v1.15.0 // indirect
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/net v0.33.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
-	google.golang.org/protobuf v1.35.2 // indirect
+	google.golang.org/genproto v0.0.0-20200825200019-8632dd797987 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
